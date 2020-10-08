6ffe49d830eeb8fe5de5967594e600563f87bb93
deepak saini <deepaksaini6413@gmail.com>
2020-10-08 15:54:16 +0530

    add role at registration page

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -38,9 +38,10 @@ class UserController extends Controller
 	            return response()->json(['errors'=>$validator->errors(),'success' => false], $this->successStatus);
 			}
 
-            if (Auth::attempt(array('name' => $request->getUser(), 'password' => md5($request->getPassword())), true)){
+            if (Auth::attempt(array('name' => $request->getUser(), 'password' => $request->getPassword()), true)){
                 $user = Auth::user(); 
-	            $token =  $user->createToken('MyApp')->accessToken; 
+                Auth::user()->roles;
+                $token =  $user->createToken('MyApp')->accessToken; 
 
 	            return response()->json(['success' => true,
 	            						 'user' => $user,
@@ -72,12 +73,15 @@ class UserController extends Controller
 	        ]);
 
 			if ($validator->fails()) { 
-	            return response()->json(['errors'=>$validator->errors()], $this->successStatus);            
+	            return response()->json(['errors'=>$validator->errors()], $this->successStatus);
 			}
 
 			$input = $request->all(); 
-			$input['password'] = bcrypt(md5($input['password'])); 
-			$input['otp'] = rand(0000,9999);
+
+			$input['password'] = bcrypt($input['password']); 
+            $input['role_id'] = (array_key_exists('role_id',$input)) ? $input['role_id'] : 3;
+			$input['otp'] = $this->generateOTP();
+            $input['account_enabled'] = 3; // Not verified user
 	        $user = User::create($input); 
 	        
 	        //Send Otp Over Mail or Phone
@@ -93,6 +97,16 @@ class UserController extends Controller
         
     }
 
+    /** 
+     * details api 
+     * 
+     * @return \Illuminate\Http\Response 
+     */ 
+    public function generateOTP(){
+        $otp = mt_rand(1000,9999);
+        return $otp;
+    }
+
 	/** 
      * details api 
      * 
@@ -104,6 +118,43 @@ class UserController extends Controller
         return response()->json(['success' => $user], $this->successStatus); 
     }
 
+    /** 
+     * Update User Profile
+     * 
+     */
+    public function updateProfile(Request $request){
+        try{
+
+            $input = $request->all();
+
+            if(array_key_exists('first_name',$input) ||
+               array_key_exists('last_name', $input) ||
+               array_key_exists('middle_name', $input) ||
+               array_key_exists('location', $input)){
+
+                $user = User::where('id', Auth::user()->id)->update($input);
+
+                if($user){
+                    $user = User::where('id',Auth::user()->id)->with('roles')->first(); 
+                    return response()->json(['success' => true,
+                                     'user' => $user,
+                                    ], $this->successStatus); 
+                }else{
+                    return response()->json(['success' => false,
+                                     'errors' => [ 'error' => 'Wrong parameters sent'],
+                                    ], $this->successStatus);
+                }
+                
+            }
+
+            return response()->json(['success' => false,
+                                     'errors' => [ 'error' => 'Wrong parameters sent'],
+                                    ], $this->successStatus); 
+
+        }catch(\Exception $e){
+            return response()->json(['success'=>false,'errors' =>['exception' => [$e->getMessage()]]], $this->successStatus); 
+        } 
+    }
     /** 
      * Import User Api
      * 
