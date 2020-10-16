7c5abcb147fce66ec4bf07890a23f372b0d7ae0d
Shubham <shubham.kumar@ibyteinfomatics.com>
2020-10-16 10:29:46 +0530

    updated app name

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -72,7 +72,7 @@
                       <!-- Menu Footer-->
                       <li class="user-footer">
                         <div class="pull-left">
-                          <a href="#" class="btn btn-default btn-flat">Profile</a>
+                          <a href="{{url('login/profile')}}" class="btn btn-default btn-flat">Profile</a>
                         </div>
                         <div class="pull-right">
                           <a class="btn btn-default btn-flat" href="{{url('logout')}}">{{ __('Logout') }}</a>
