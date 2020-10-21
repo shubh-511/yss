e751965195738b2da62e9c43647b289e53994318
Shubham <shubham.kumar@ibyteinfomatics.com>
2020-10-21 16:17:14 +0530

    update counsellor login api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -64,9 +64,9 @@ class UserController extends Controller
                     //echo $url; die; 
 
                     $cURL = $this->url_get_contents($url); 
-                    //$cURL = json_decode($cURL, true);
-                    return $cURL; 
-
+                    $cURL = json_decode($cURL, true);
+                    //return $cURL; 
+echo $cURL['status']; die;
                     //$json = file_get_contents($url);
                     //$cURL = json_decode($json, true);
 
@@ -118,9 +118,9 @@ class UserController extends Controller
         }else{
             $url_get_contents_data = false;
         }
-            //return $url_get_contents_data;
-            $userData = json_decode($url_get_contents_data, true);
-            return $userData;
+            return $url_get_contents_data;
+            //$userData = json_decode($url_get_contents_data, true);
+            //return $userData;
         
     }
 
