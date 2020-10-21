7725890fa9c4e49b1269ddeb0c29ac049a4e9081
Shubham <shubham.kumar@ibyteinfomatics.com>
2020-10-21 14:17:35 +0530

    added phone update api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -546,7 +546,7 @@ class UserController extends Controller
 
     public function sendSMS($otp, $countryCode, $phone)
     {
-        echo env('ACCOUNT_SID'); die;
+        return env('ACCOUNT_SID'); die;
         $sid = env('ACCOUNT_SID'); // Your Account SID from www.twilio.com/console
         $token = env('AUTH_TOKEN'); // Your Auth Token from www.twilio.com/console
         $message = $otp." is one time otp to verify your phone number";
