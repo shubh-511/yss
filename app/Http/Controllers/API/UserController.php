292da99a611723822bf64dcbfa74cf08ea8d137a
Shubham <shubham.kumar@ibyteinfomatics.com>
2020-10-21 11:44:51 +0530

    usercontroller api changes

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -102,23 +102,25 @@ class UserController extends Controller
 
     public function url_get_contents($url) 
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-        $data = curl_exec($ch);
-        $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        if ($retcode == 200) {
-            //return $data;
-            $responseData = json_decode($data, TRUE);
-            return $responseData;
-        } else {
-            return null;
-    }
+        if (function_exists('curl_exec')){ 
+        $conn = curl_init($url);
+        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($conn, CURLOPT_FRESH_CONNECT,  true);
+        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+        $url_get_contents_data = (curl_exec($conn));
+        curl_close($conn);
+        }elseif(function_exists('file_get_contents')){
+            $url_get_contents_data = file_get_contents($url);
+        }elseif(function_exists('fopen') && function_exists('stream_get_contents')){
+            $handle = fopen ($url, "r");
+            $url_get_contents_data = stream_get_contents($handle);
+        }else{
+            $url_get_contents_data = false;
+        }
+            //return $url_get_contents_data;
+            $userData = json_decode($url_get_contents_data, true);
+            return $userData;
+        
     }
 
     /** 
