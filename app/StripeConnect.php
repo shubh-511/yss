40121e019203edf1f3478ee87cd2f16630e1c79f
John <shubham.594k@gmail.com>
2020-10-26 12:19:38 +0530

    added stripe connect api

--- app/StripeConnect.php
+++ app/StripeConnect.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class StripeConnect extends Model
 {
-    
+    protected $table = 'stripe_connect';
     protected $fillable = [
         'user_id', 
         'stripe_id', 
