3d69c7fe12ee14999d667e8e1f5ddcb860a1cbaf
John <shubham.594k@gmail.com>
2020-10-28 14:46:08 +0530

    added split time api and added booking module

--- app/Http/Controllers/API/PackageController.php
+++ app/Http/Controllers/API/PackageController.php
@@ -266,6 +266,7 @@ class PackageController extends Controller
             $package = Package::where('id', $request->package_id)->where('user_id', $request->user_id)->first();
 
             $sessionTime = $package->session_minutes;
+            //$sessionTime = 23;
 
 
             $myAvailableHours = AvailaibleHours::where('availability_id', $getAvailability->id)->get();
