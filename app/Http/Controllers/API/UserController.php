78bdf52b0bd1634b6151fb1100a83709b06783ed
Shubham <shubham.kumar@ibyteinfomatics.com>
2021-02-04 10:45:26 +0530

    update image size while uploading

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -397,7 +397,7 @@ class UserController extends Controller
         try{
 
             $validator = Validator::make($request->all(), [ 
-                'image' => 'required|mimes:jpeg,png,jpg|max:2048',  
+                'image' => 'required|mimes:jpeg,png,jpg|max:10240',  
                 
             ]);
 
