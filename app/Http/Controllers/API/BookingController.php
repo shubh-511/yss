1c6bc6c031b39357f974b75004015bad51110770
Shubham <shubham.kumar@ibyteinfomatics.com>
2021-02-10 10:49:19 +0530

    update get booking api

--- app/Http/Controllers/API/BookingController.php
+++ app/Http/Controllers/API/BookingController.php
@@ -757,13 +757,13 @@ class BookingController extends Controller
             $user = Auth::user();
             if($user->role_id == 2)
             {
-              $booking = Booking::with('counsellor:id,name,email')->with('package')->with('user:id,name,email')->where('counsellor_id', $user->id)->orderBy('booking_date', 'DESC')
+              $booking = Booking::with('counsellor:id,name,email,timezone')->with('package')->with('user:id,name,email')->where('counsellor_id', $user->id)->orderBy('booking_date', 'DESC')
                 ->withTrashed()
                 ->get();
             }
             else
             {
-              $booking = Booking::with('counsellor:id,name,email')->with('package')->with('user:id,name,email')->where('user_id', $user->id)->orderBy('booking_date', 'DESC')
+              $booking = Booking::with('counsellor:id,name,email,timezone')->with('package')->with('user:id,name,email')->where('user_id', $user->id)->orderBy('booking_date', 'DESC')
               ->withTrashed()
               ->get();
             }
